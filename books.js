a198e25e45e8919b1eb75aefa8873350f3669d00
Marcus Ang <marcusajh0802@gmail.com>
2023-07-04 11:00:05 +0800

    add cards

--- books.js
+++ books.js
@@ -8,6 +8,8 @@ let form = document.querySelector("#book-form");
 form.addEventListener("submit", function(){
     event.preventDefault();
     addBookToLibrary();
+    let newBookForm = document.querySelector("#new-book-form");
+    newBookForm.classList.add("d-none");
 })
 
 let myLibrary = []
@@ -19,6 +21,25 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function render() {
+    let libraryEl = document.querySelector("#library");
+    libraryEl.innerHTML = "";
+    for (let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i];
+        let bookEl = document.createElement("div");
+        bookEl.innerHTML = `
+        <div class="card col-md-2 mb-4" style="width: 18rem;">
+            <div class="card-body text-center">
+                <h3 class="card-title">${book.title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${book.author}</h6>
+                <p class="card-text">${book.pages}</p>
+            </div>
+        </div>
+        `
+        libraryEl.appendChild(bookEl);
+    }
+}
+
 function addBookToLibrary(event) {
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
@@ -26,5 +47,6 @@ function addBookToLibrary(event) {
     let read = document.querySelector("#read").checked;
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
-    console.log(myLibrary);
+    render();
+
 }
